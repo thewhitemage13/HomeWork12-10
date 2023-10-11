7b77a95e1b12f043b4d4f813ded1da096ea7d8cc
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-10-11 12:23:13 +0200

    Add files via upload

---
+++ HomeWork2(12.10)/HomeWork2(12.10).cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int degreeofnumber(int number, int degree) {
+   
+    if (degree == 0)return 1;
+    return number * degreeofnumber(number, degree - 1);
+}
+int main() {
+    int number;
+    int degree;
+    cout << "enter a number: ";
+    cin >> number;
+    cout << "enter the degree: ";
+    cin >> degree;
+    int result = degreeofnumber(number, degree);
+    cout << result;
+}
---
+++ HomeWork3(12.10)/HomeWork3(12.10).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int Sum(int a, int b) 
+{    
+    if (a > b)return 0;
+    return a + Sum(a + 1, b);
+}
+
+int main() {
+    int a;
+    int b;
+    cout << "Enter the first number in the range: ";
+    cin >> a;
+    cout << "Enter a lower number in the range of: ";
+    cin >> b;
+    int sum = Sum(a,b);
+    cout << sum;
+}
